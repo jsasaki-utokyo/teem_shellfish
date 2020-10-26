680177c97a7c603b1aa1b38b116133efe3e30f5e
jsasaki <jsasaki.ece@gmail.com>
2020-10-26 22:41:41 +0900

    Name changed. Add include file.

---
+++ shellfish_params.h
@@ -0,0 +1,6 @@
+! module shellfish に含める定数をこのファイル中で定義し，module shellfish の include で読み込む
+
+! Params for function update_mu()
+  real(rp), parameter :: mu0 = 0.0_rp  ! constant mortality
+
+! 関数別に必要な定数を定義する
